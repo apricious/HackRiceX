70f329f67c4b231cdee8d858ffdd7138f973be5e
mshef1101 <mshef1101@gmail.com>
2020-09-19 15:38:28 -0500

    help

---
+++ phpFile.php
@@ -0,0 +1,23 @@
+<?php
+    $serverName = "localhost";
+    $username = "username";
+    $password = "password";
+
+    $connection = new mysql($serverName, $username, $password);
+
+
+    if ($connection -> connect_error){
+        die("Connection failed: " . $connection -> connect_error);
+    }
+
+    $sql = "CREATE DATABASE potato";
+
+    if ($connection -> query($sql) === TRUE) {
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database" . $connection -> error;
+    }
+
+    $connection -> close();
+
+?>
